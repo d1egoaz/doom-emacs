1279ee917bc11cb357c5051140726814939e9fb4
Diego Alvarez <diego.canada@icloud.com>
2020-12-15 15:36:14 -0800

    Actually open rss/elfeed in its own workspace
    I basically looked at the calendar app and copy/pasted the code into the rss app.
    
    I tested it on my machine and it worked.

--- modules/app/rss/autoload.el
+++ modules/app/rss/autoload.el
@@ -1,8 +1,19 @@
 ;;; app/rss/autoload.el -*- lexical-binding: t; -*-
 
 ;;;###autoload
-(defalias '=rss #'elfeed
-  "Activate (or switch to) `elfeed' in its workspace.")
+(defun =rss ()
+  "Activate (or switch to) `elfeed' in its workspace."
+  (interactive)
+  (if (featurep! :ui workspaces)
+      (progn
+        (+workspace-switch "rss" t)
+        (doom/switch-to-scratch-buffer)
+        (elfeed)
+        (+workspace/display))
+    (setq +rss--wconf (current-window-configuration))
+    (delete-other-windows)
+    (switch-to-buffer (doom-fallback-buffer))
+    (elfeed)))
 
 ;;;###autoload
 (defun +rss/delete-pane ()
@@ -77,7 +88,9 @@
       (with-current-buffer b
         (remove-hook 'kill-buffer-hook #'+rss-cleanup-h :local)
         (kill-buffer b)))
-    (mapc #'kill-buffer show-buffers)))
+    (mapc #'kill-buffer show-buffers))
+  (if (featurep! :ui workspaces)
+      (+workspace/delete "rss")))
 
 
 ;;
